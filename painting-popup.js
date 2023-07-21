51283a4484440a2d68eae69aa17729a04dc43c0a
Varun  Mangalick <varunmangalick@Varuns-MacBook-Air.local>
2023-07-21 18:04:19 -0400

    refactor

---
+++ painting-popup.js
@@ -0,0 +1,12 @@
+function Popup(title, author, file, description, dir) {
+  $("#painting-title").text(title)
+  $("#painting-author").text(author)
+  $("#painting-description").text(description)
+  $("#painting").attr("src", "paintings/" + file + ".jpg")
+  if (dir === "V") {
+    $("#modal-dia").removeClass("modal-lg");
+  } else {
+    $("#modal-dia").addClass("modal-lg");
+  }
+  $('#infoModal').modal('show');
+}
